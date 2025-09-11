7015af04d804893d8bf093e663164b8c90e30be3
ArindaAsiimwe <aritah185@gmail.com>
2025-09-11 13:46:05 +0300

    Fix the tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,9 @@
+import pluginVue from 'eslint-plugin-vue'
+
 export default [
+    // For JavaScript files
     {
+        files: ['**/*.js'],
         languageOptions: {
             globals: {
                 window: "readonly",
@@ -13,6 +17,23 @@ export default [
             "no-console": "warn",
             "no-unused-vars": "error",
             "semi": ["error", "always"]
+        }
+    },
+    // For Vue files
+    ...pluginVue.configs['flat/essential'],
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            globals: {
+                window: "readonly",
+                document: "readonly",
+                console: "readonly"
+            }
+        },
+        rules: {
+            "no-console": "warn",
+            "no-unused-vars": "error",
+            "vue/multi-word-component-names": "off"
         }
     }
 ];
