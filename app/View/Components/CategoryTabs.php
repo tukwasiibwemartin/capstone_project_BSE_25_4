d0d5022d2822c04485584e80558ad7696470fd36
ArindaAsiimwe <aritah185@gmail.com>
2025-10-23 19:05:57 +0300

    Fix code quality

--- app/View/Components/CategoryTabs.php
+++ app/View/Components/CategoryTabs.php
@@ -12,7 +12,9 @@ class CategoryTabs extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct() {}
+    public function __construct()
+    {
+    }
 
     /**
      * Get the view / contents that represent the component.
